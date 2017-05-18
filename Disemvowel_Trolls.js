7b14a227f3b78c64278af2d02751e69c37b4ad79
kyle <kyleflemington@gmail.com>
2017-05-18 07:25:59 -0700

    XO

--- Disemvowel_Trolls.js
+++ Disemvowel_Trolls.js
@@ -14,6 +14,6 @@ function disemvowel(str) {
 //2. ES6 - Let: Declare all vowels, upper and lowercase
 //3. ES6 - Let: Declare empty string, used to push vowel-less sentence
 //4. Standard for loop, starts at 0, does not exceed length of string, increments by 1
-//5. When looping over the string, if an indecy  This method returns -1 if the value to search for never occurs.
+//5. If an indecy matches any element in the vowel array...indexOf(This method returns -1 if the value to search for never occurs)
 //6. Push any non-vowels into the emptry string solution
 //9. Loop ends, return the final string
---
+++ exes_and_ohs.js
@@ -0,0 +1,16 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contains any char.
+
+// Examples input/output:
+
+function XO(str) {
+    let exes = 0;
+    let ohs = 0;
+    for( var i = 0; i < str.length; i++; ) {
+      if ( str[i] == 'x') {
+        exes++;
+      } else if ( str[i] == 'o') {
+        ohs++
+      }
+    }
+   return exes === ohs
+}
