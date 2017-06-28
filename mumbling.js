ad55c313c7b2ef669e2ea14869ebb5efb53e05ff
kyle <kyleflemington@gmail.com>
2017-06-28 06:51:24 -0700

    Kota Practice

--- mumbling.js
+++ mumbling.js
@@ -1,4 +1,7 @@
 function accum(s) {
+	let newAr = [];
 	for(var = 0; i < s.length; i++) {
+		newAr.push(s[i])
   }
+  console.log(newAr)
 }
---
+++ summation.js
@@ -0,0 +1,8 @@
+var summation = function (num) {
+  for(var i = 1, total = 0; i <= num; i++) {
+    total += i;
+  }
+  return total
+}
+
+//For each number in the loop add to variable 'total'
